ec13b528c104d563237f670cebfa94ddf23178ea
avalianiz <avalianizurab2006@gmail.com>
2025-10-29 16:42:36 +0400

    built distance matrix computed pairwise distances using the matrix norm and normalized the distances to ensure dbscan works

--- audioclustering.py
+++ audioclustering.py
@@ -27,3 +27,28 @@ for file in audio_files:
 print(f"Loaded {len(spectograms)} spectograms.")
 
 # compute pairwise distances using matrix norm
+
+def matrix_distance(A,B, norm_type="fro"):
+    # compute distance between two spectograms using Frobenius
+    # resize the smallest common shape
+    min_rows = min(A.shape[0], B.shape[0])
+    min_cols = min(A.shape[1], B.shape[1])
+
+    A_cut = A[:min_rows, :min_cols]
+    B_cut = B[:min_rows, :min_cols]
+    diff = A_cut - B_cut
+    return np.linalg.norm(diff, ord=norm_type)
+
+# Build distance matrix
+n = len(spectograms)
+D = np.zeros((n,n))
+for i in range(n):
+    for j in range(n):
+        if i < j:
+            D[i,j] = matrix_distance(spectograms[i], spectograms[j])
+            D[j,i] = D[i,j]
+
+# Normalize distances for dbscan
+D = D / np.max(D)
+
+# cluster using dbscan
